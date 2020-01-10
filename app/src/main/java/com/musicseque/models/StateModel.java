46096868e8b87935abe402868295ab0f55f52c9f
shobin <shobinjindal91@gmail.com>
2020-01-10 18:25:27 +0530

    10-01-2020()

---
+++ app/src/main/java/com/musicseque/models/StateModel.java
@@ -0,0 +1,22 @@
+package com.musicseque.models;
+
+public class StateModel {
+    String StateName;
+    String StateId;
+
+    public String getStateName() {
+        return StateName;
+    }
+
+    public void setStateName(String stateName) {
+        StateName = stateName;
+    }
+
+    public String getStateId() {
+        return StateId;
+    }
+
+    public void setStateId(String stateId) {
+        StateId = stateId;
+    }
+}
